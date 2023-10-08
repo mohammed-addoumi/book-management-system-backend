7b74a65e2e43289ec3c1c777bc8d6c8e9cd16d5f
Mohammed Addoumi <mohammedaddoumi@hotmail.Fr>
2023-10-08 17:19:49 +0100

    project creation using spring initiliazer

---
+++ src/main/java/com/mytech/bookmanagementsystembackend/BookManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mytech.bookmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytech/bookmanagementsystembackend/BookManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytech.bookmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
